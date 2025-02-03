13b2ca5cf603abdc6afde3be1adfe5cef6c627aa
Esteban Hood <ehood50@gmail.com>
2025-02-03 01:38:18 -0300

    Crea funcion actualizarLista

--- app.js
+++ app.js
@@ -3,19 +3,22 @@ let amigos = [];
 function agregarAmigo(){
    let nombreAmigo = document.getElementById("amigo");
    let nombre = nombreAmigo.value.trim();
-    //console.log(nombre);
-    amigos.push(nombre);
-    //console.log(amigos);
-    
+        
     if (nombre) { // Verifica que no esté vacío
         amigos.push(nombre); // Agregar al array
         nombreAmigo.value = ""; // Limpiar el input
 
-        // Mostrar en la lista
-        let lista = document.getElementById("listaAmigos");
-        let nuevoItem = document.createElement("li");
-        nuevoItem.textContent = nombre;
-        lista.appendChild(nuevoItem);
+        actualizarLista();//llama la funcion que crea la lista de amigos
+        
     } else {
         alert("Ingrese un texto válido."); }
  }
+function actualizarLista(){
+    let lista = document.getElementById("listaAmigos"); // Selecciona la lista en el HTML
+    lista.innerHTML = ""; // Limpia la lista antes de actualizar
+    for (let i = 0; i < amigos.length; i++) {//Recorre el array amigos
+        let li = document.createElement("li"); // Crea un nuevo elemento <li>
+        li.textContent = amigos[i]; // Asigna el nombre del amigo al <li>
+        lista.appendChild(li); // Agrega el <li> a la lista en el HTML
+    }
+}
